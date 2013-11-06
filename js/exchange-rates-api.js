93fc37b2d08dca4167e74be1cc586f066a654e5c
Padma <ppriya.v@gmail.com>
2013-11-05 20:47:18 -0800

    Added amount, default currencies and rounded the rate to 2 decimals

--- js/exchange-rates-api.js
+++ js/exchange-rates-api.js
@@ -9,6 +9,9 @@ $(function(){
 	var newBaseRate = 0;
 	var exchangeRate = 0;
 	var finalRate = 0;
+	var amount = 1;
+	
+	$('#amount').val(1);
 	//Get the list of currencies
 	// Use jQuery.ajax to get the latest exchange rates, with JSONP:
 	$.ajax({
@@ -24,6 +27,8 @@ $(function(){
 			$.each( json, function( key, val ) {
 				$(".currencies").append("<option value='"+key+"'>"+val+", "+key+"</option>");				
 			});
+			$("#base-currency").val("USD");
+			$("#exchange-currency").val("EUR");
 			
 		}
 	});
@@ -33,7 +38,7 @@ $(function(){
 		e.preventDefault();
 		newBase = $('#base-currency').val();
 		exchange = $('#exchange-currency').val();
-		$('#result').remove();
+		$('#result').remove();		
 		//console.log("base is: "+newBase+", exchange is: "+exchange);
 		$.ajax({
 			url: 'http://openexchangerates.org/api/latest.json?app_id=8ff908b6ac944637ae64d7a02f88a57f',
@@ -46,13 +51,19 @@ $(function(){
 					if (key == exchange)
 						exchangeRate = val;
 					//console.log("rate key is: "+key+", rate value is: "+val);						
-				});					
+				});				
 				
-				finalRate = (1/newBaseRate) * exchangeRate;
+				amount = $('#amount').val();				
+				finalRate = (1/newBaseRate) * exchangeRate * amount;
+				
 				console.log("final rate is: "+finalRate);
-				$("body").append("<div id='result'><h2>Exchange rate is: "+finalRate+"</h2></div>");				
+				$("body").append("<div id='result'><h2>"+amount+" "+newBase+" = "+finalRate.toFixed(2)+" "+exchange+"</h2></div>");				
 			}
 		});
 	});
 
 });
+//round the decimals: toFixed(NUMBER of DECIMALS); (finalRate = finalRate.toFixed(2);)
+//1 base currency = 'x' exchange currency
+//amount of base currency = "Y" exchange currency
+//default USD
